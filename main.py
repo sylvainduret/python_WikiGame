c47069e99dca805f6222587c7a7f5f317f768dec
sylvainduret <56431581+sylvainduret@users.noreply.github.com>
2021-01-21 11:09:32 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -4,13 +4,4 @@ import tkinter
 from tkinter import *
 
 if __name__ == '__main__':
-    l=["coucou","compté du danmark de mes 2","ok pt'it test tranquillou","fred le 100"]
-    fenetre= tkinter.Tk()
-    test ='tsfdhgjvgnfbvdfsq'
-    #label =tkinter.Label(fenetre,text="Bonjour voici votre Objectif!").pack()
-    for i in range(0,len(l)):
-        bouton=tkinter.Button(fenetre, text=l[i]).pack(side=LEFT)
-
-
-    #bouton=tkinter.Button(fenetre, text="New Game", command=fenetre.).pack()
-    fenetre.mainloop()
+    
